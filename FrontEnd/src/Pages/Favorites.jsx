b1b2c256b4c0f01090b715461eda9221dc4ed43f
Keanu Rouss <timajori1@gmail.com>
2025-07-25 09:33:45 -0400

    comments

--- FrontEnd/src/Pages/Favorites.jsx
+++ FrontEnd/src/Pages/Favorites.jsx
@@ -7,16 +7,16 @@ import PlantCardContainer from "../components/PlantCardContainer";
 
 
 function Favorites() {
-   const [favorites, setFavorites] = useState([])
-const location = useLocation()
-console.log(location.state)
+   const [favorites, setFavorites] = useState([]) //a variable with an empty array
+const location = useLocation() //location object
+console.log(location.state) //pinpoint location
 
-  useEffect(()=>{
+  useEffect(()=>{ //update the list using real data/in that case using my test data
     async function getdata() {
      const response = await fetch (`http://localhost:8080/api/users/${location.state.userId}`)
-     const data = await response.json()
+     const data = await response.json() // return an response in a format that is pretty to read
      console.log(data) 
-     setFavorites(data)
+     setFavorites(data) //generate the data for favorite/ update data when clicked
     }
     getdata()
   }, [])
@@ -25,19 +25,22 @@ console.log(location.state)
 
   return (
     <div>
-      <NavBar userId= {location.state.userId}/>
+      {/* get plant id passed from in/follow their location while they are navigating. */}
+      <NavBar userId= {location.state.userId}/> 
       <div className='Favoriteview'>
       <h2>My Favorite Plants</h2>
       <PlantCardContainer>
        {favorites.map((plant) => ( // userId={location.state._id} that was a prop but it was causing home page to crash and not show.
           <PlantCard key={plant.id}  userId={location._id} plant={plant}  />
+          //
         )
         )}
-        </PlantCardContainer>
+        </PlantCardContainer> 
         </div>
       </div>
   )
 }
+// const style to style the whole cards easily
 const styles = {
 plantCardContainer: {
     display: 'flex',
--- FrontEnd/src/Pages/PlantDetail.jsx
+++ FrontEnd/src/Pages/PlantDetail.jsx
@@ -6,12 +6,13 @@ import NavBar from "../components/NavBar";
 
 function PlantDetail() {
   const { plantId } = useParams();
-  const [plant, setPlant] = useState(null);
+  const [plant, setPlant] = useState(null);// set to start out with empty data because didn't know what 
+  // was included from the api response.
   const [error, setError] = useState("");
 
 
   useEffect(() => {
-    const fetchPlant = async () => {
+    const fetchPlant = async () => { //
       try {
         // const res = await fetch(
         //  `https://www.perenual.com/api/v2/species/details/${plantId}?key=${import.meta.env.VITE_APIKEY}`
@@ -22,7 +23,7 @@ function PlantDetail() {
         setPlant(plantDetailData);
       } catch (err) {
         console.error("Error:", err);
-        setError(err.message);
+        setError(err.message); //return an error
       }
     };
 
@@ -39,18 +40,19 @@ console.log(plant.default_image.medium_url)
       
       <NavBar/>
       <div className="Plantdetailview"/>
-      <h2>{plant.common_name}</h2>
+      {/* plant.common_name was to retrieve that name as there was a scientific name that was way to hard 
+      to pronounce and long long. */}
+      <h2>{plant.common_name}</h2>  
       <p><strong>Type:</strong> {plant.type}</p>
       <p><strong>Watering:</strong> {plant.watering}</p>
       <p><strong>Sunlight:</strong> {plant.sunlight.join(", ")}</p>
       <p>{plant.description}</p>
       <img
+      // selected the default image instead of the large, thumbnail etc
         src={plant.default_image.medium_url}
         alt={plant.common_name}
         style={{ maxWidth: "200px", borderRadius: "10px", marginTop: "1rem" }}
         />
-
-      {/* render more plant details here */}
     </div>
   );
 }
